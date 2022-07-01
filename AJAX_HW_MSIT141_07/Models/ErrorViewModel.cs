85a17e7b793df27c07c7e15c286e9677ed0e6f80
張正昱 <ihtgschen@gmail.com>
2022-07-01 21:43:47 +0800

    加入專案檔案。

---
+++ AJAX_HW_MSIT141_07/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AJAX_HW_MSIT141_07.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
